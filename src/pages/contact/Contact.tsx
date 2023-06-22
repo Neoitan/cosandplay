21d396608d734696a1c01b1bcc0f70d7287c9603
Antoine <antoine.levas@gmail.com>
2023-06-23 00:17:31 +0200

    Add start contact pages

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -8,33 +8,46 @@ import twitter from "../../assets/icons/twitter.svg";
 
 const Contact = () => {
   return (
-    <div className="bodyhp">
+    <div className="bodycontact">
       <div className="body">
         <Header selectedTabIndex={0} contactus={true} />
-        <div className="homepage">
-          <div className="homepage_content">
-            <div>
-              <div className="homepage_content_subtitle">BIENVENUE CHEZ</div>
-              <div className="homepage_content_title">COS AND PLAY</div>
+        <div className="contact">
+          <div className="contact_header">
+            <div className="contact_header_title">
+              Vous souhaitez nous contacter ?
             </div>
-            <div className="homepage_content_links">
-              <div className="homepage_content_links_sn">
-                <img
-                  className="homepage_content_links_sn grow"
-                  src={facebook}
-                  alt=""
-                />
-                <img
-                  className="homepage_content_links_sn grow"
-                  src={instagram}
-                  alt=""
-                />
-                <img
-                  className="homepage_content_links_sn grow"
-                  src={twitter}
-                  alt=""
-                />
-              </div>
+            <div className="contact_header_subtitle">
+              Vous avez dans l’intention d’organiser un événement, un
+              rassemblement, une convention ou même une simple rencontre ? Alors
+              n’hésitez pas à remplir le formulaire si joint pour nous contacter
+              !
+            </div>
+          </div>
+          <div className="contact_form">
+            <div className="contact_form_block">
+              <label className="contact_form_block_idName">Votre nom</label>
+              <input
+                type="text"
+                className="contact_form_block_field text"
+              ></input>
+            </div>
+            <div className="contact_form_block">
+              <label className="contact_form_block_idName">Votre email</label>
+              <input
+                type="text"
+                className="contact_form_block_field text"
+              ></input>
+            </div>
+            <div className="contact_form_block">
+              <label className="contact_form_block_idName">Votre objet</label>
+              <input
+                type="text"
+                className="contact_form_block_field text"
+              ></input>
+            </div>
+            <div className="contact_form_block">
+              <label className="contact_form_block_idName">Votre message</label>
+              <textarea className="contact_form_block_field message"></textarea>
             </div>
           </div>
         </div>
